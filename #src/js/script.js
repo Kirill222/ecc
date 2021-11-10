b6b4af25ff8e0b23ea8a8132290084d3e4cd7bcb
John Connor <zkmd2012@inbox.ru>
2021-11-10 19:52:10 +0500

    Nav menu done

--- #src/js/script.js
+++ #src/js/script.js
@@ -1,1 +1,6 @@
-@@include("alert.js");
+const toggleButton = document.getElementsByClassName("sandwich")[0]
+const navigation = document.getElementsByClassName("header-middle")[0]
+
+toggleButton.addEventListener("click", ()=> {   
+    navigation.classList.toggle('toggle-menu')
+})
